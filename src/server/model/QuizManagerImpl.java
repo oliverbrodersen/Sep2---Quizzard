e9ff21bd84bf892903af52c80edd7500277c186e
Benjamin <benjamin.preece@hotmail.com>
2020-04-01 13:27:47 +0200

    QuizManagerImpl got connected to Subject

--- src/server/model/QuizManagerImpl.java
+++ src/server/model/QuizManagerImpl.java
@@ -1,5 +1,26 @@
 package server.model;
 
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
 public class QuizManagerImpl implements QuizManager
 {
+  private PropertyChangeSupport support;
+
+  public QuizManagerImpl()
+  {
+    support = new PropertyChangeSupport(this);
+  }
+
+  @Override public void addListener(String eventName,
+      PropertyChangeListener listener)
+  {
+    support.addPropertyChangeListener(eventName, listener);
+  }
+
+  @Override public void removeListener(String eventName,
+      PropertyChangeListener listener)
+  {
+    support.removePropertyChangeListener(eventName, listener);
+  }
 }
