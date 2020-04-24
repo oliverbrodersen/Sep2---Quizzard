8abec1f9feea21f04252b0ba182126c0a425363d
Sockom <57130573+Sockom@users.noreply.github.com>
2020-04-24 09:59:47 +0200

    The last two

--- src/client/views/questionview/QuestionVM.java
+++ src/client/views/questionview/QuestionVM.java
@@ -1,4 +1,12 @@
 package client.views.questionview;
 
+import client.model.QuizConverter;
+
 public class QuestionVM {
+
+  private QuizConverter quizConverter;
+
+  public QuestionVM(QuizConverter quizConverter) {
+    this.quizConverter = quizConverter;
+  }
 }
